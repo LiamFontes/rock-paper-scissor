933bee6ddc331d860c5d58b94edcf39b516739c1
LiamFontes <liamfontes1244@gmail.com>
2021-06-21 21:55:05 -0400

    completed Rock paper scissors game with full functionality. Could use additional formatting to spruce up. But okay for now

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+function playRound(playerSelection) {
+    if (document.getElementById("computerScore").textContent === "5" || document.getElementById("playerScore").textContent === "5"){
+        ResetScore()  
+        return
+    }
+    let playChoice = null;
+    let winner = null;
+    if (playerSelection === "rockImage") {
+        playChoice = "rock";
+    }
+
+    if (playerSelection === "paperImage") {
+        playChoice = "paper";
+    }
+
+    if (playerSelection === "scissorImage") {
+        playChoice = "scissor";
+    }
+
+    const rockOption = "rock";
+    const scissorOption = "scissor";
+    const paperOption = "paper";
+
+    computerChoice = computerPlay();
+
+    if (playChoice === computerChoice) {
+    result = `Both chose ${playChoice}, try to outsmart the machine!`;
+}
+
+/* USER CHOOSES SCISSOR*/
+else if (playChoice === scissorOption && computerChoice === rockOption) {
+    result = "Rock crushes scissors! The computer beat you this round."
+    winner = "computer";
+}
+
+else if (playChoice === scissorOption && computerChoice === paperOption) {
+    result = "Scissor cuts paper! Point to humankind!"
+    winner = "player";
+}
+
+/* USER CHOOSES ROCK*/
+else if (playChoice === rockOption && computerChoice === paperOption) {
+    result = "Paper covers rock! This round goes to the machine!"
+    winner = "computer";
+}
+
+else if (playChoice === rockOption && computerChoice === scissorOption) {
+    result = "Rock smashes scissors! Take that computer!"
+    winner = "player";
+}
+
+/* USER CHOOSES PAPER*/
+else if (playChoice === paperOption && computerChoice === scissorOption) {
+    result = "Scissor cuts Paper! Point for Skynet"
+    winner = "computer";
+}
+
+else if (playChoice === paperOption && computerChoice === rockOption) {
+    result = "Paper covers rock! Chalk one up for humanity"
+    winner = "player";
+}
+incrementScore(winner)
+MessageSend(result)
+maxScoreCheck()
+}
+
+
+
 function computerPlay() {
 
     let choice = null;
@@ -19,89 +87,46 @@ function computerPlay() {
 
 }
 
-const computerChoice = computerPlay();
 
-function playRound(playerSelection, computerSelection) {
-    playerSelectionMod = playerSelection.toLowerCase();
-    let winner = null;
-    const rockOption = "rock";
-    const scissorOption = "scissor";
-    const paperOption = "paper";
-
-
-    if (playerSelectionMod === computerSelection) {
-        result = "Tied Round! Try again"
+function incrementScore(winner){
+    if (winner === "player"){
+        const playerScore = document.getElementById("playerScore").innerText;
+        const updatedScore = new String(Number(playerScore) + 1);
+        document.getElementById("playerScore").textContent = updatedScore;
+        
     }
 
-    /* USER CHOOSES SCISSOR*/
-    else if (playerSelectionMod === scissorOption && computerSelection === rockOption) {
-        result = "Rock crushes scissors! The computer beat you this round."
-        winner = "computer";
-    }
-
-    else if (playerSelectionMod === scissorOption && computerSelection === paperOption) {
-        result = "Scissor cuts paper! Point to humankind!"
-        winner = "player";
-    }
-
-    /* USER CHOOSES ROCK*/
-    else if (playerSelectionMod === rockOption && computerSelection === paperOption) {
-        result = "Paper covers rock! This round goes to the machine!"
-        winner = "computer";
-    }
-
-    else if (playerSelectionMod === rockOption && computerSelection === scissorOption) {
-        result = "Rock smashes scissors! Take that computer!"
-        winner = "player";
-    }
-
-    /* USER CHOOSES PAPER*/
-    else if (playerSelectionMod === paperOption && computerSelection === scissorOption) {
-        result = "Scissor cuts Paper! Point for Skynet"
-        winner = "computer";
-    }
-
-    else if (playerSelectionMod === paperOption && computerSelection === rockOption) {
-        result = "Paper covers rock! Chalk one up for humanity"
-        winner = "player";
-    }
-
-    console.log(result)
-
-    return winner
-}
-
-function playGame() {
-    const maxScore = 5;
-    let playerScore = 0;
-    let computerScore = 0;
-    for (i = 0; i < 100; i++) {
-        let playerSelection = prompt("Rock, paper, or scissors?")
-        let roundResult = playRound(playerSelection, computerPlay());
-        console.log(roundResult)
-        if (roundResult === "player") {
-            playerScore += 1;
-        }
-
-        else if (roundResult === "computer") {
-            computerScore += 1;
-        }
-        console.log(`The current score is Humans-${playerScore} : Computers-${computerScore}`);
-
-        if (playerScore === Math.ceil(maxScore / 2)) {
-            console.log("Humankind wins!");
-            break;
-        }
-
-
-        if (computerScore === Math.ceil(maxScore / 2)) {
-            console.log("The computer wins!");
-            break;
-        }
-
-
+    if (winner === "computer"){
+        const computerScore = document.getElementById("computerScore").innerText;
+        const updatedScore = new String(Number(computerScore) + 1);
+        document.getElementById("computerScore").textContent = updatedScore;
+        
     }
 }
 
+function ResetScore(){
 
-playGame();
+    document.getElementById("playerScore").textContent = "0";
+    document.getElementById("computerScore").textContent = "0";
+    if (document.getElementById("computerScore").textContent !== "5" && document.getElementById("playerScore").textContent !== "5"){
+        document.getElementById("messageDiv").textContent = "And so it begins...";    
+    }
+    
+
+}
+
+function maxScoreCheck(){
+
+    if(document.getElementById("playerScore").innerText == "5"){
+        document.getElementById("messageDiv").textContent = "Humankind Wins! Reset the score to show that silicon who's boss!";
+    }
+    if(document.getElementById("computerScore").innerText == "5"){
+        document.getElementById("messageDiv").textContent = "Skynet is live... Reset the score to give humanity another chance!";
+    
+    }
+}
+
+function MessageSend(result){
+    document.getElementById("messageDiv").textContent = result;
+
+}
